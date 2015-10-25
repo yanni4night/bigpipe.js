a7ca3e81880f82a43c3cf4700291ade362b0d709
yinyong <yanni4night@gmail.com>
2015-10-25 20:59:14 +0800

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,40 @@
 /**
-  * Copyright (C) 2015 yanni4night.com
-  * Gruntfile.js
-  *
-  * changelog
-  * 2015-10-22[18:19:17]:revised
-  *
-  * @author yanni4night@gmail.com
-  * @version 0.1.0
-  * @since 0.1.0
-  */
+ * Copyright (C) 2015 yanni4night.com
+ * Gruntfile.js
+ *
+ * changelog
+ * 2015-10-22[18:19:17]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 0.1.0
+ * @since 0.1.0
+ */
 
-module.exports = function(grunt){
-    require('time-grunt')(grunt);
-    require('load-grunt-tasks')(grunt);
-    
-    grunt.registerTask('test',[]);
-    grunt.registerTask('default',['test']);
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    clean: {
+      all: ['./bigpipe.*.js{,.map}']
+    },
+    browserify: {
+      bigpipe: {
+        src: 'src/bigpipe.js',
+        dest: './bigpipe.src.js'
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      bigpipe: {
+        src: './bigpipe.src.js',
+        dest: './bigpipe.min.js'
+      }
+    }
+  });
+
+  grunt.registerTask('test', []);
+  grunt.registerTask('default', ['clean', 'browserify', 'uglify', 'test']);
 };
